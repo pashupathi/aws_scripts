8dc99cb0c76627eb8db681485cb42f2e9ad31f12
pashupathi <pashupathi@users.noreply.github.com>
2017-09-23 00:40:28 +0530

    Update jsoncsv.sh

--- jsoncsv.sh
+++ jsoncsv.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -
-cat params.json |sed 's|[{}]||g'|sed 's|\"||g'|sed 's/[][]//g'|sed 's|:\ |=|g'|sed '/^\s*$/d'|tr -d "\012"|sed -e 's/ //g' > stage.csv
+cat params.json |sed 's|[{}]||g'|sed 's|\"||g'|sed 's/[][]//g'|sed 's|:\ |=|g'|sed '/^\s*$/d'|tr -d "\012"|sed -e 's/ //g'|sed 's|,|, |g' > stage.csv
 params=$(cat stage.csv)
-echo -n "${params}" >> change.properties 
+if [ $(cat change.properties |wc -w) = 0 ]; then
+        echo -n "${params}" >> change.properties
+else
+        echo -n ", " >> change.properties
+        echo -n "${params}" >> change.properties
+fi
+sed -i ':a;N;$!ba;s/\n//g' change.properties
 rm stage.csv
 
